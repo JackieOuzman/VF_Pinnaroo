2b189520d2c8f40ea7e415984aa3359c7199a354
Jackie Ouzman <jackie.ouzman@csiro.au>
2021-12-10 14:53:39 +1030

    Just mapped a small amount of the animal movment data

--- temp_getting_data_spatial.R
+++ temp_getting_data_spatial.R
@@ -1,35 +1,95 @@
-#```{r write up GPS plots workings, message=FALSE, warning=FALSE, include=FALSE}
 
-#need to make df into spatial object for graphing and try clipping to paddock boundary
-# long_plains <- st_read("W:/VF/LongPlain/LP Blk Bound/LongPlain_GDA_internal_bound.shp")
-# long_plains_Vf <- st_read("W:/VF/LongPlain/LP Blk Bound/LongPlainVF_bound.shp")
-# long_plains_Vf_area <- st_read("W:/VF/LongPlain/LP Blk Bound/LongPlainVF_area_bound.shp")
-
-pinnaroo_Vf_area <- st_read("W:/VF/Pinnaroo/activation_VF/Pinnaroo_VF_clip.shp")
-
-#str(week1_2_3)
+library(dplyr)
+library(tidyverse)
+library(readr)
+library(lubridate)
+library(DT)
+library(sp)
 
 
-week1_2_3_sf <-
-  st_as_sf(week1_2_3,
-           coords = c("gpsData.lng", "gpsData.lat"),
-           crs = 4326,
-           agr = "constant")
+library(rgdal)
+library(sf)
 
-week1_2_3_sf_trans <-
-  st_transform(week1_2_3_sf, crs = 28354)
-week1_2_3_sf_clip <-
-  st_intersection(week1_2_3_sf_trans, long_plains)
+#install.packages("plotKML")
+library(plotKML)
+library(knitr)
+library(png)
+
+library(readxl)
 
 
-fence1_GPS <- week1_2_3_sf_clip %>% 
-  filter(fencesID_1 == "fence1")  
-fence2_GPS <- week1_2_3_sf_clip %>% 
-  filter(fencesID_1 == "fence2")
-fence3_GPS <- week1_2_3_sf_clip %>% 
-  filter(fencesID_1 == "fence3") 
-fence3_deactive_GPS <- week1_2_3_sf_clip %>% 
-  filter(fencesID_1 == "deactive_VF3") 
+library(ggmap)
+library(maps)
+library(mapdata)
+
+library(raster)
+#library(hms) Add these before I use them?
+#library(plyr)
+
+library(formattable)
+library(ggpubr)
+
+
+
+
+##################################################################################
+####### subset data to VF area (based on when fenceID was active)   ##############
+##################################################################################
+fence1_GPS <- animal_GPS_data_sf_trans_clip %>% 
+  filter(VF_area == "VF_area1")  
+fence2_GPS <- animal_GPS_data_sf_trans_clip %>% 
+  filter(VF_area == "VF_area2")
+fence3_GPS <- animal_GPS_data_sf_trans_clip %>% 
+  filter(VF_area == "VF_area3") 
+fence4_GPS <- animal_GPS_data_sf_trans_clip %>% 
+  filter(VF_area == "VF_area4") 
+
+
+#Check below plots are checks
+
+
+ggplot() +
+  geom_sf(data = pinnaroo_paddock_area, color = "black", fill = NA) +
+  geom_sf(data = pinnaroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = fence1_GPS ,alpha = 0.01) +
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  facet_wrap(VF_area~ fencesID)
+
+
+ggplot() +
+  geom_sf(data = pinnaroo_paddock_area, color = "black", fill = NA) +
+  geom_sf(data = pinnaroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = fence2_GPS ,alpha = 0.01) +
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  facet_wrap(VF_area~ fencesID)
+
+ggplot() +
+  geom_sf(data = pinnaroo_paddock_area, color = "black", fill = NA) +
+  geom_sf(data = pinnaroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = fence3_GPS ,alpha = 0.01) +
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  facet_wrap(VF_area~ fencesID)
+
+
+ggplot() +
+  geom_sf(data = pinnaroo_paddock_area, color = "black", fill = NA) +
+  geom_sf(data = pinnaroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = fence4_GPS ,alpha = 0.01) +
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  facet_wrap(VF_area~ fencesID)
+
+
+
+##########################################################################################################
+
 
 
 
@@ -38,27 +98,25 @@ fence3_deactive_GPS <- week1_2_3_sf_clip %>%
 fence1_day1 <-filter(fence1_GPS, day_since_vf_start == 1)
 fence2_day1 <-filter(fence2_GPS, day_since_vf_start == 1)
 fence3_day1 <-filter(fence3_GPS, day_since_vf_start == 1)
-fence3_deactivated_day1 <-filter(fence3_deactive_GPS, day_since_vf_start == 1)
+fence4_day1 <-filter(fence4_GPS, day_since_vf_start == 1)
 
 
 
 ## just want the first 8 hours from each fence change
 min(fence1_day1$local_time)+hours(8)
-fence1_day1 <- fence1_day1 %>% filter(local_time <= ymd_hms("2020-10-21 23:07:00", tz= "Australia/Adelaide"))
+fence1_day1 <- fence1_day1 %>% filter(local_time <= ymd_hms("2021-10-07 23:59:00", tz= "Australia/Adelaide"))
 
 min(fence2_day1$local_time)+hours(8)
-fence2_day1 <- fence2_day1 %>% filter(local_time <= ymd_hms("2020-10-25 18:57:00", tz= "Australia/Adelaide"))
+fence2_day1 <- fence2_day1 %>% filter(local_time <= ymd_hms("2021-10-11 20:33:00", tz= "Australia/Adelaide"))
 
 min(fence3_day1$local_time)+hours(8)
-fence3_day1 <- fence3_day1 %>% filter(local_time <= ymd_hms("2020-10-30 16:07:00", tz= "Australia/Adelaide"))
+fence3_day1 <- fence3_day1 %>% filter(local_time <= ymd_hms("2021-10-12 17:03:00", tz= "Australia/Adelaide"))
 
-min(fence3_deactivated_day1$local_time)+hours(8)
-fence3_deactivated_day1 <- fence3_deactivated_day1 %>% 
-  filter(local_time <= ymd_hms("2020-11-03 20:07:00", tz= "Australia/Adelaide"))
-
+min(fence4_day1$local_time)+hours(8)
+fence4_day1 <- fence4_day1 %>%  filter(local_time <= ymd_hms("2021-10-17 17:33:00", tz= "Australia/Adelaide"))
 
 
-fence1_3_deactice3_day1 <- rbind(fence1_day1, fence2_day1,fence3_day1, fence3_deactivated_day1 )
+
 
 fence1_day1 <-  fence1_day1 %>% 
   mutate(day_since_trial_start = 1)
@@ -66,44 +124,80 @@ fence1_day1 <-  fence1_day1 %>%
 fence2_day1 <-  fence2_day1 %>% 
   mutate(day_since_trial_start = 5)
 
-fence3_day1 <-  fence1_day1 %>% 
-  mutate(day_since_trial_start = 10)
+fence3_day1 <-  fence3_day1 %>% 
+  mutate(day_since_trial_start = 6)
 
-fence3_deactivated_day1 <-  fence1_day1 %>% 
-  mutate(day_since_trial_start = 14)
-
-fence1_3_deactice3_day1 <-  fence1_3_deactice3_day1 %>% 
-  mutate(day_since_trial_start = case_when(
-    date == "2020-10-21" ~ 1,
-    date == "2020-10-25" ~ 5,
-    date == "2020-10-30" ~ 10,
-    date == "2020-11-03" ~ 14
-  ))
-
-# get the control mob only
+fence4_day1 <-  fence4_day1 %>% 
+  mutate(day_since_trial_start = 11)
 
 
-week1_2_3_sf_clip_control <- week1_2_3_sf_clip %>% 
-  filter(VF_name == "Control")
-
-#What are the dates for day 1 with the VF mob use these dates to filter the control mob
-#unique(fence1_3_deactice3_day1$date)
+fence1_4_day1 <- rbind(fence1_day1, fence2_day1,fence3_day1, fence4_day1 )
 
 
-#list_of_date <- list(unique(fence1_3_deactice3_day1$date)) #I dont know why this wont work??
-#test <- week1_2_3_sf_clip_control %>% filter(date  %in% list_of_date)
+fence1_4_day1_select_days <-
+  fence1_4_day1 %>% filter(date  == "2021-10-07" |
+                           date  == "2021-10-11" |
+                           date  == "2021-10-12" |
+                           date  == "2021-10-17")
 
-week1_2_3_sf_clip_control_day1 <- week1_2_3_sf_clip_control %>% filter(date  == "2020-10-21" |
-                                                                         date  == "2020-10-25" |
-                                                                         date  == "2020-10-30" |
-                                                                         date  == "2020-11-03")
 
-week1_2_3_sf_clip_control_day1 <- week1_2_3_sf_clip_control_day1 %>% 
-  mutate(day_since_trial_start = case_when(
-    date == "2020-10-21" ~ 1,
-    date == "2020-10-25" ~ 5,
-    date == "2020-10-30" ~ 10,
-    date == "2020-11-03" ~ 14
-  ))
+#```
+str(fence1_4_day1_select_days)
+unique(fence1_4_day1_select_days$day_since_trial_start)
+
+fence1_4_day1_select_days %>% 
+group_by(day_since_trial_start) %>% 
+  summarise(min_time = min(local_time, na.rm= TRUE),
+            max_time = max(local_time, na.rm= TRUE)) %>% 
+  arrange(min_time)
+
+
+#### Animal movement day 1 of each fence move for 8 hours
+
+#```{r write up GPS plots image, echo=TRUE, message=FALSE, warning=FALSE}
+
+
+
+unique(pinnaroo_Vf_area$day_since_trial_start)
+
+
+ggplot() +
+  geom_sf(data = pinnaroo_paddock_area, color = "black", fill = NA) +
+  geom_sf(data = pinnaroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = fence1_4_day1_select_days ,alpha = 0.01) +
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  #labs(title= "Day1 of VF")+
+  facet_wrap(.~ day_since_trial_start)
+
+##########################################################################
+
+#For each above plot Dana would like to know how what is the time window.
+
+#```{r write up GPS plots time period, echo=TRUE, message=FALSE, warning=FALSE}
+str(fence1_4_day1_select_days)
+fence1_4_day1_select_days_time <- fence1_4_day1_select_days %>%
+  dplyr::select(VF_area,local_time ) %>% 
+  group_by(VF_area) %>% 
+  summarise(min_local_time = min(local_time),
+            max_local_time = max(local_time))
+fence1_4_day1_select_days_time
+
+fence1_4_day1_select_days_time <- as.data.frame(fence1_4_day1_select_days_time)
+
+
+fence1_4_day1_select_days_time <-fence1_4_day1_select_days_time %>% 
+  dplyr::select(VF_area,min_local_time, max_local_time )  %>% 
+  arrange(min_local_time)
+
+
+
+fence1_4_day1_select_days_time <- fence1_4_day1_select_days_time %>% 
+  mutate(difference_in_time = max_local_time - min_local_time)
+
+fence1_4_day1_select_days_time
 
 ```
+
+
